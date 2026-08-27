1b48926f918bf9162c9a424cc590ac4deb5cbd72
@Xieling-123 <xieling00315@163.com>
2026-08-27 12:00:41 +0800

    Auto backup on 2026-08-27 12:00:40

--- updata_github.bat
+++ updata_github.bat
@@ -1,7 +1,11 @@
 @echo off
 cd /d D:\mytool\01_doc\snippet
+
+:: 获取标准时间
+for /f "delims=" %%i in ('powershell -Command "Get-Date -Format 'yyyy-MM-dd HH:mm:ss'"') do set "NOW=%%i"
+
 git pull origin master
 git add .
-git commit -m "日常更新 [%date% %time%]"
+git commit -m "Auto backup on %NOW%"
 git push origin master
 pause
